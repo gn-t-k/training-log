3dbb51b85e8dd6a35b23b0521b43aaa1619ec4d9
gn-t-k <gntk1995422@gmail.com>
2023-01-05 17:28:13 +0900

    部位のcrud

--- src/pages/hello/index.page.tsx
+++ src/pages/hello/index.page.tsx
@@ -1,10 +1,9 @@
 import { NextPage } from "next";
-import { FC } from "react";
+import { FC, MouseEventHandler, useEffect } from "react";
 
-import { useTraineeState } from "@/libs/recoil/trainee";
+import { trpc } from "@/libs/trpc/client/trpc";
 
 import { RequireLogin } from "@/features/auth/require-login/require-login";
-import { useSessionContext } from "@/features/auth/session-context/session-context";
 
 const HelloContainer: NextPage = () => {
   return (
@@ -16,13 +15,45 @@ const HelloContainer: NextPage = () => {
 export default HelloContainer;
 
 const Hello: FC = () => {
-  const session = useSessionContext();
-  const trainee = useTraineeState();
+  const muscleRegisterMutator = trpc.muscle.register.useMutation();
+  const musclesQuery = trpc.muscle.getAll.useQuery();
 
-  return (
-    <>
-      <p>{JSON.stringify(session)}</p>
-      <p>{JSON.stringify(trainee)}</p>
-    </>
-  );
+  useEffect(() => {
+    if (muscleRegisterMutator.status === "success") {
+      musclesQuery.refetch();
+    }
+  }, [muscleRegisterMutator.status, musclesQuery]);
+
+  const onClick: MouseEventHandler = async (e) => {
+    e.preventDefault();
+    muscleRegisterMutator.mutate({ name: `部位${String(new Date())}` });
+  };
+
+  if (muscleRegisterMutator.status === "loading") {
+    <p>部位を登録中</p>;
+  }
+
+  switch (musclesQuery.status) {
+    case "loading":
+      return <p>部位を取得中</p>;
+    case "success": {
+      return musclesQuery.data.length === 0 ? (
+        <div>
+          <button {...{ onClick }}>登録</button>
+          <p>まだ部位が登録されていません</p>
+        </div>
+      ) : (
+        <div>
+          <button {...{ onClick }}>登録</button>
+          <ul>
+            {musclesQuery.data.map((muscle) => (
+              <li key={muscle.id}>{muscle.name}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+    case "error":
+      return <p>部位の取得に失敗しました</p>;
+  }
 };
