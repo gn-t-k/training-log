a073072e2b94f612faa13f42a139e1d10ca8eafb
gn-t-k <gntk1995422@gmail.com>
2023-01-27 17:33:51 +0900

    get id

--- src/features/exercise/use-get-exercise-id.ts
+++ src/features/exercise/use-get-exercise-id.ts
@@ -3,15 +3,18 @@ import { z } from "zod";
 
 type UseGetExerciseId = () => string | null;
 export const useGetExerciseId: UseGetExerciseId = () => {
-  const [_first, second, third, ..._rest] = useRouter().asPath.split("/");
+  const [_first, second, third, fourth, ..._rest] =
+    useRouter().asPath.split("/");
   const validPathsSchema = z.object({
-    second: z.literal("exercises"),
-    third: z.string().min(1),
+    second: z.literal("settings"),
+    third: z.literal("exercises"),
+    fourth: z.string().min(1),
   });
   const parseResult = validPathsSchema.safeParse({
     second,
     third,
+    fourth,
   });
 
-  return parseResult.success ? parseResult.data.third : null;
+  return parseResult.success ? parseResult.data.fourth : null;
 };
