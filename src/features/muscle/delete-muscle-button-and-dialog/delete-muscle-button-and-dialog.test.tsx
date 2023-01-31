d10027139b1f12f9774a7dbdb4fea836277406a2
gn-t-k <gntk1995422@gmail.com>
2023-02-01 07:30:21 +0900

    部位の削除

---
+++ src/features/muscle/delete-muscle-button-and-dialog/delete-muscle-button-and-dialog.test.tsx
@@ -0,0 +1,16 @@
+import { composeStories } from "@storybook/testing-react";
+import { render, screen } from "@testing-library/react";
+
+import * as stories from "./delete-muscle-button-and-dialog.stories";
+
+describe("DeleteMuscleButtonAndDialog", () => {
+  const Stories = composeStories(stories);
+
+  describe("初期状態", () => {
+    beforeEach(() => {
+      render(<Stories.Default />);
+    });
+
+    test.todo("", () => {});
+  });
+});
--- src/libs/prisma/commands/delete-muscle-command.ts
+++ src/libs/prisma/commands/delete-muscle-command.ts
@@ -2,7 +2,6 @@ import prisma from "../client";
 
 import type { Muscle } from "@/features/muscle/muscle";
 
-
 export type DeleteMuscleCommand = (props: { muscle: Muscle }) => Promise<void>;
 export const deleteMuscleCommand: DeleteMuscleCommand = async (props) => {
   await prisma.muscle.delete({
