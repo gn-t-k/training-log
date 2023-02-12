7af78667247ea5c9f4537c9c3044ffe9e6c78295
gn-t-k <gntk1995422@gmail.com>
2023-02-13 07:23:52 +0900

    テスト

--- src/features/training/edit-training-button-and-modal/edit-training-button-and-modal.stories.tsx
+++ src/features/training/edit-training-button-and-modal/edit-training-button-and-modal.stories.tsx
@@ -1,12 +1,7 @@
-import { action } from "@storybook/addon-actions";
-import { ComponentMeta, ComponentStoryObj } from "@storybook/react";
-import { within } from "@storybook/testing-library";
-import userEvent from "@testing-library/user-event";
-import { ComponentProps, FC } from "react";
-
-import { sleep } from "@/utils/sleep";
-
 import { EditTrainingButtonAndModalView } from "./edit-training-button-and-modal";
+
+import type { ComponentMeta, ComponentStoryObj } from "@storybook/react";
+import type { ComponentProps, FC } from "react";
 
 type Meta = ComponentMeta<typeof EditTrainingButtonAndModalView>;
 type Props = ComponentProps<typeof EditTrainingButtonAndModalView>;
@@ -19,10 +14,50 @@ export default componentMeta;
 
 const Wrapper: FC<Partial<Props>> = (props) => {
   const args: Props = {
-  }
+    training: props.training ?? {
+      id: "id-t-1",
+      createdAt: new Date(),
+      records: [
+        {
+          id: "id-r-1",
+          exercise: {
+            id: "id-e-1",
+            name: "ベンチプレス",
+            targets: [
+              {
+                id: "id-m-1",
+                name: "大胸筋",
+              },
+              {
+                id: "id-m-2",
+                name: "上腕三頭筋",
+              },
+              {
+                id: "id-m-3",
+                name: "三角筋前部",
+              },
+            ],
+          },
+          memo: "重かった",
+          sets: [
+            {
+              id: "id-s-1",
+              weight: 80,
+              repetition: 10,
+            },
+            {
+              id: "id-s-2",
+              weight: 80,
+              repetition: 10,
+            },
+          ],
+        },
+      ],
+    },
+  };
 
-  return <EditTrainingButtonAndModalView {...args} />
-}
+  return <EditTrainingButtonAndModalView {...args} />;
+};
 
 const Template: Story = {
   render: (props: Partial<Props>) => {
--- src/features/training/edit-training-button-and-modal/edit-training-button-and-modal.test.tsx
+++ src/features/training/edit-training-button-and-modal/edit-training-button-and-modal.test.tsx
@@ -1,5 +1,5 @@
 import { composeStories } from "@storybook/testing-react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
 
 import * as stories from "./edit-training-button-and-modal.stories";
 
@@ -11,6 +11,6 @@ describe("EditTrainingButtonAndModal", () => {
       render(<Stories.Default />);
     });
 
-    test.todo("", () => {});
+    test.todo("テスト");
   });
 });
